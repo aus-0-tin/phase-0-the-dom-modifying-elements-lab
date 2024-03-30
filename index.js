23e33b4bb0a36bf596b75b349a95eb4e518fbca2
aus-0-tin <austin.waithaka@student.moringaschool.com>
2024-03-30 20:40:23 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+const mainNode = document.getElementById('main');
+mainNode.remove();
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.setAttribute('id', 'victory');
+
+
+const yourName = 'YOUR-NAME';
+newHeader.textContent = `${yourName} is the champion`;
+
+
+document.body.appendChild(newHeader);
